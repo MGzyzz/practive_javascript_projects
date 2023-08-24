58b07d51f8e279779c688bda711473cd69771fd8
Madi <madi_gaziz@mail.ru>
2023-08-25 03:22:01 +0600

    Выполнил 1 задачу с проверкой на каждый элемент массива

---
+++ task_one.js
@@ -0,0 +1,9 @@
+massive = ['молоко', 'пиво', 'пиво', 'молоко', 'молоко']
+
+for (i = 0; i < massive.length; i++) {
+    if (massive[i] == 'пиво') {
+        console.log('плохо')
+    } else {
+        console.log('хорошо')
+    }
+}
